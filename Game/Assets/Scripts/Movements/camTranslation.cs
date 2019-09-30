12c0ddfdf3b4712670ff22616962e44d7a0c30ba
Antoine <bouchez.antoine@outlook.fr>
2019-09-30 17:40:23 +0200

    Monday-17:39
    - road repeating
    - bikehandlebar picture
    - mouse event (turn the handle bar)

---
+++ Game/Assets/Scripts/Movements/camTranslation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camTranslation : MonoBehaviour
+{
+    Rigidbody cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.velocity = new Vector3(0, 0, 2);
+    }
+}
