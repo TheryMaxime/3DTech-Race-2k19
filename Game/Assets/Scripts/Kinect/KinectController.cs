b93b9624b6856d6f105053dc4c1fa7fcd00aeb2e
TheryMaxime <thery_maxime@yahoo.fr>
2019-09-30 15:07:28 +0200

    Adding first scene

---
+++ Game/Assets/Scripts/Kinect/KinectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinectController : MonoBehaviour
+{
+    public int m_CharacterId; //ID of the current character
+
+
+    private int m_PlayerId; //ID of the current player
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the current character controller
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        KinectManager manager = KinectManager.Instance;
+
+    }
+}
