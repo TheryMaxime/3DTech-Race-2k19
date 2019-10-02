3bf52cbe72ad9a5292d8808d740eeeaceee66c48
Antoine <bouchez.antoine@outlook.fr>
2019-10-02 12:21:16 +0200

    gamecontroller_modifs
    roads instantiated at start()

--- Game/Assets/Scripts/gameController.cs
+++ Game/Assets/Scripts/gameController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class gameController : MonoBehaviour
 {
+    public GameObject[] roadsPrefabs;
+    private GameObject[] roads;
+
+    private GameObject character;
+
 
     public static gameController instance;
 
@@ -22,7 +27,14 @@ public class gameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GameObject.FindGameObjectWithTag("Player");
+        float z = 0f;
+        roads = new GameObject[roadsPrefabs.Length];
+        for(int i = 0; i < roadsPrefabs.Length; i++)
+        {
+            roads[i] = Instantiate(roadsPrefabs[i], new Vector3(0,0,z), Quaternion.identity) as GameObject;
+            z += roads[i].transform.Find("cube_road").GetComponent<BoxCollider>().size.z;
+        }
     }
 
     // Update is called once per frame
