92aaf1bb7c7d612d3aafe6b05475d90e90f25e08
TheryMaxime <thery_maxime@yahoo.fr>
2019-10-03 17:28:23 +0200

    Adding roads assets
    + changing character collider & rigidbody to character controller

--- Game/Assets/Scripts/Movements/CharacterMovement.cs
+++ Game/Assets/Scripts/Movements/CharacterMovement.cs
@@ -9,20 +9,22 @@ public class CharacterMovement : MonoBehaviour
     public float m_speed = 0.01f;
     public Camera m_camera;
 
-    private Rigidbody m_Rigidbody;
+    //private Rigidbody m_Rigidbody;
+    private CharacterController m_Rigidbody;
     private Transform m_transform;
 
     // Start is called before the first frame update
     void Awake()
     {
-        this.m_Rigidbody = GetComponent<Rigidbody>();
+        //this.m_Rigidbody = GetComponent<Rigidbody>();
+        this.m_Rigidbody = GetComponent<CharacterController>();
         this.m_transform = GetComponent<Transform>();
     }
 
     private void OnEnable()
     {
-        m_Rigidbody.isKinematic = false;
-        this.m_Rigidbody.velocity = (new Vector3(0, 0, this.m_speed));
+        //m_Rigidbody.isKinematic = false;
+        //this.m_Rigidbody.velocity = (new Vector3(0, 0, this.m_speed));
     }
 
     private void Update()
@@ -30,7 +32,8 @@ public class CharacterMovement : MonoBehaviour
         //this.m_Rigidbody.MovePosition(new Vector3(0, 0, this.m_speed));
         //this.m_Rigidbody.velocity = (new Vector3(0, 0, this.m_speed));
         Vector3 movement = this.m_transform.forward * this.m_speed;
-        this.m_Rigidbody.MovePosition(this.m_Rigidbody.position + movement);
+        //this.m_Rigidbody.MovePosition(this.m_Rigidbody.position + movement);
+        this.m_Rigidbody.Move(movement);
     }
 
     /*private void Turn(float angle)
@@ -53,7 +56,9 @@ public class CharacterMovement : MonoBehaviour
         Quaternion turnRotation = Quaternion.Euler(0f, angle*this.m_TurnSpeed, 0f);
 
         // Apply this rotation to the rigidbody's rotation.
-        this.m_Rigidbody.MoveRotation(this.m_Rigidbody.rotation * turnRotation);
-        this.m_camera.transform.rotation = Quaternion.Euler(0f, 0f, -angle);
+        //this.m_Rigidbody.MoveRotation(this.m_Rigidbody.rotation * turnRotation);
+        //this.m_Rigidbody.attachedRigidbody.MoveRotation(this.m_camera.transform.rotation * turnRotation);
+        this.transform.Rotate(0f, angle * this.m_TurnSpeed, 0f);
+        //this.m_camera.transform.rotation = Quaternion.Euler(0f, 0f, -angle);
     }
 }
