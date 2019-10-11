8455f4f34b618dc7e2200d54636620edc4f9d278
Antoine <bouchez.antoine@outlook.fr>
2019-10-11 08:46:39 +0200

    comments Antoine

--- Game/Assets/Scripts/Menu/gameRules.cs
+++ Game/Assets/Scripts/Menu/gameRules.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class gameRules : MonoBehaviour
 {
+	// when the associated button is clicked, the rules scene is launched
     public void showRules()
     {
         SceneManager.LoadScene("RulesScene", LoadSceneMode.Single);
--- Game/Assets/Scripts/gameControl/increaseScore.cs
+++ Game/Assets/Scripts/gameControl/increaseScore.cs
@@ -8,7 +8,9 @@ public class increaseScore : MonoBehaviour
     {
         if (other.GetComponent<CharacterController>() != null)
         {
+        	//when the character avoid an obstacle, the score is increased (see scored() function in the game controller)
             gameController.instance.scored();
+            // the avoided obstacle is destroyed
             Destroy(this);
         }
     }
